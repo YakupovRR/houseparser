6bf98a801e06cafc809cf6b3e45016b496d06c36
Ruslan <specter63@gmail.com>
2023-04-09 17:46:23 +0400

    Парсер проектов

---
+++ src/main/java/ru/samarahouse/houseparser/HouseparserApplication.java
@@ -0,0 +1,13 @@
+package ru.samarahouse.houseparser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HouseparserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HouseparserApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/samarahouse/houseparser/HouseparserApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.samarahouse.houseparser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HouseparserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
